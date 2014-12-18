057ce4d082f251a2d5746990e1e87057ea456e69
Florian Rey <florian.rey@elao.com>
2014-12-18 14:11:13 +0100

    Handle pools

--- index.js
+++ index.js
@@ -2,27 +2,42 @@
 
 module.exports = function(gulp) {
 
+    // Check gulp injection
+    if ((typeof gulp !== 'object') || (gulp.constructor.name !== 'Gulp')) {
+        console.error('Error: You must pass a gulp instance as first parameter !');
+        process.exit(1);
+    }
+
     // Assets
     var
-        Assets = require('./lib/Assets'),
-        assets = new Assets();
+        Assets     = require('./lib/Assets'),
+        AssetsPool = require('./lib/AssetsPool'),
+        assets     = new Assets();
+
+    assets
+        .addPool(new AssetsPool('test_a', 'test/test/a', 'Teeeeest AAA'))
+        .addPool(new AssetsPool('test_b', 'test/test/b'))
+        .addPool(new AssetsPool('test_c', 'test/test/c', 'Teeeeest CCC'));
 
     // Gulp
     var
-        util = require('gulp-util');
+        gutil = require('gulp-util');
 
-    // Gulp - Pools
+    // Task - Pools
     gulp.task('pools', function(callback) {
         var
             pools = assets.getPools();
 
-        util.log(
-            'Found',
-            util.colors.cyan(pools.length),
-            'pools'
-        );
-        
-        console.log(pools);
+        gutil.log('Found', gutil.colors.cyan(pools.length), 'pools');
+
+        pools.forEach(function(pool) {
+            gutil.log('-', pool.getName(), gutil.colors.magenta(pool.getPath()));
+            if (pool.hasComment()) {
+                gutil.log(' ', gutil.colors.cyan(pool.getComment()));
+            }
+        });
+
+        callback();
     });
 
     return assets;
