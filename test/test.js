1be4c8e353413cc7223fc5fe286eb3b12b482ae1
Florian Rey <florian.rey@elao.com>
2014-12-19 09:28:35 +0100

    Fix tests

--- test/test.js
+++ test/test.js
@@ -12,15 +12,20 @@ var
 
 describe('AssetsPool', function() {
     var
-        assetsPool = new AssetsPool('foo', 'bar');
-    describe('#getName()', function() {
-        it('should return name', function() {
-            assert.equal('foo', assetsPool.getName());
+        assetsPool = new AssetsPool('foo', 'bar', 'foobar');
+    describe('#getId()', function() {
+        it('should return id', function() {
+            assert.equal('foo', assetsPool.getId());
         });
     });
     describe('#getPath()', function() {
         it('should return path', function() {
             assert.equal('bar', assetsPool.getPath());
+        });
+    });
+    describe('#getDescription()', function() {
+        it('should return description', function() {
+            assert.equal('foobar', assetsPool.getDescription());
         });
     });
 });
