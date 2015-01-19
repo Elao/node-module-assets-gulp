6a4a5580b09b065b9532c39d3fe1c915e33d7945
nervo <nervo@nervo.net>
2015-01-19 20:40:09 +0100

    Cleanup

--- test/plugins.js
+++ test/plugins.js
@@ -13,7 +13,7 @@ describe('Plugins', function() {
 
     // Clean
     describe('clean', function() {
-        
+
         before(function() {
             // Create assets dir
             if (!fs.existsSync('test/fixtures/web/assets')) {
@@ -27,12 +27,12 @@ describe('Plugins', function() {
             fs.writeFileSync('test/fixtures/web/assets/_test', 'test');
             fs.writeFileSync('test/fixtures/web/assets/test/_test', 'test');
         });
-        
+
         it('should run without errors', function(done) {
             var
                 assets = require('..')({cwd: 'test/fixtures'});
 
-            require('../plugins/clean')(assets)['task'](done);
+            require('../plugins/clean')(assets).task(done);
         });
 
         it('should have cleaned dest path', function(done) {
