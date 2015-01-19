7d5c3d49ea159900fa9ed44e0701abfe7c1224b6
nervo <nervo@nervo.net>
2015-01-19 20:40:53 +0100

    Handle different lists using cli parameters

--- plugins/list.js
+++ plugins/list.js
@@ -4,70 +4,72 @@
 module.exports = function(assets)
 {
     return {
-
-        // List bundles
-        bundles: function(callback) {
+        // Task
+        task: function(callback) {
             var
                 gulpUtil = require('gulp-util'),
-                bundles  = assets.bundles.find();
+                bundles, libraries;
 
-            gulpUtil.log('Found', gulpUtil.colors.cyan(bundles.length), 'bundles');
+            // Parameters
+            if (!gulpUtil.env.bundles && !gulpUtil.env.libraries && !gulpUtil.env.pools) {
+                gulpUtil.env.bundles   = true;
+                gulpUtil.env.libraries = true;
+                gulpUtil.env.pools     = true;
+            }
 
-            bundles.forEach(function(bundle) {
-                gulpUtil.log('-', bundle.getId(), gulpUtil.colors.magenta(bundle.getPath()));
-                if (bundle.hasDescription()) {
-                    gulpUtil.log(' ', gulpUtil.colors.cyan(bundle.getDescription()));
-                }
-            });
+            // List bundles
+            if (gulpUtil.env.bundles) {
+                bundles = assets.bundles.find();
 
-            callback();
-        },
+                gulpUtil.log('Found', gulpUtil.colors.cyan(bundles.length), 'bundles');
 
-        // List libraries
-        libraries: function(callback) {
-            var
-                gulpUtil  = require('gulp-util'),
+                bundles.forEach(function(bundle) {
+                    gulpUtil.log('-', bundle.getId(), gulpUtil.colors.magenta(bundle.getPath()));
+                    if (bundle.hasDescription()) {
+                        gulpUtil.log(' ', gulpUtil.colors.cyan(bundle.getDescription()));
+                    }
+                });
+            }
+
+            // List libraries
+            if (gulpUtil.env.libraries) {
                 libraries = assets.libraries.find();
 
-            gulpUtil.log('Found', gulpUtil.colors.cyan(libraries.length), 'libraries');
+                gulpUtil.log('Found', gulpUtil.colors.cyan(libraries.length), 'libraries');
 
-            libraries.forEach(function(library) {
-                gulpUtil.log('-', gulpUtil.colors.magenta(library.getPath()));
-                if (library.hasDescription()) {
-                    gulpUtil.log(' ', gulpUtil.colors.cyan(library.getDescription()));
-                }
-            });
-
-            callback();
-        },
-
-        // List pools
-        pools: function(callback) {
-            var
-                gulpUtil = require('gulp-util');
-
-            gulpUtil.log('Added', gulpUtil.colors.cyan(assets.poolHandlers.length), 'pool handlers');
-
-            // Pool Handlers
-            assets.poolHandlers.forEach(function(poolHandler)
-            {
-                var
-                    pools;
-
-                gulpUtil.log('-', poolHandler.getId());
-                if (poolHandler.hasDescription()) {
-                    gulpUtil.log(' ', gulpUtil.colors.cyan(poolHandler.getDescription()));
-                }
-
-                // Pools
-                pools = poolHandler.pools.find();
-                gulpUtil.log('     Found', gulpUtil.colors.cyan(pools.length), 'pools');
-                pools.forEach(function(pool) {
-                    gulpUtil.log('     -', pool.getName());
-                    gulpUtil.log('      ', gulpUtil.colors.cyan('src: '), gulpUtil.colors.magenta(pool.getSrc()));
-                    gulpUtil.log('      ', gulpUtil.colors.cyan('dest:'), gulpUtil.colors.magenta(pool.getDest()));
+                libraries.forEach(function(library) {
+                    gulpUtil.log('-', gulpUtil.colors.magenta(library.getPath()));
+                    if (library.hasDescription()) {
+                        gulpUtil.log(' ', gulpUtil.colors.cyan(library.getDescription()));
+                    }
                 });
-            });
+            }
+
+            // List pools
+            if (gulpUtil.env.pools) {
+                gulpUtil.log('Added', gulpUtil.colors.cyan(assets.poolHandlers.length), 'pool handlers');
+
+                // Pool Handlers
+                assets.poolHandlers.forEach(function(poolHandler)
+                {
+                    var
+                        pools;
+
+                    gulpUtil.log('-', poolHandler.getId());
+                    if (poolHandler.hasDescription()) {
+                        gulpUtil.log(' ', gulpUtil.colors.cyan(poolHandler.getDescription()));
+                    }
+
+                    // Pools
+                    pools = poolHandler.pools.find();
+                    gulpUtil.log('     Found', gulpUtil.colors.cyan(pools.length), 'pools');
+                    pools.forEach(function(pool) {
+                        gulpUtil.log('     -', pool.getName());
+                        gulpUtil.log('      ', gulpUtil.colors.cyan('src: '), gulpUtil.colors.magenta(pool.getSrc()));
+                        gulpUtil.log('      ', gulpUtil.colors.cyan('dest:'), gulpUtil.colors.magenta(pool.getDest()));
+                    });
+                });
+            }
 
             callback();
         }
