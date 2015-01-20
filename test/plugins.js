c5af473b980a3f3de6f6daa054534738a1237f09
nervo <nervo@nervo.net>
2015-01-20 21:42:12 +0100

    Fonts plugin tests

--- test/plugins.js
+++ test/plugins.js
@@ -46,4 +46,56 @@ describe('Plugins', function() {
         });
     });
 
+    // Fonts
+    describe('fonts', function() {
+        var
+            gulp   = require('gulp'),
+            assets = require('..')({
+                cwd: cwd,
+                silent: true
+            });
+
+        require('../layouts/npm')(assets);
+        require('../layouts/bower')(assets);
+        require('../layouts/assets')(assets);
+        require('../layouts/symfony')(assets);
+        require('../layouts/components')(assets);
+
+        require('../plugins/fonts')(assets, gulp).task();
+
+        after(function(done) {
+            require('../plugins/clean')(assets).task(done);
+        });
+
+        it('should have handled assets', function() {
+            assert.deepEqual(
+                fs.readdirSync(assets.getPoolHandler('fonts').getDestPath()), [
+                    'a.ttf',
+                    'foo',
+                    'z.ttf'
+                ]
+            );
+            assert.deepEqual(
+                fs.readdirSync(assets.getPoolHandler('fonts').getDestPath('foo')), [
+                    'a.ttf',
+                    'z.ttf'
+                ]
+            );
+        });
+
+        describe('no assets', function() {
+            var
+                assets = require('..')({
+                    cwd: cwd,
+                    silent: true
+                });
+
+            it('should return null', function() {
+                assert.isNull(
+                    require('../plugins/fonts')(assets, gulp).task()
+                );
+            });
+        });
+    });
+
 });
