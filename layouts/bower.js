dc6fe94a5a69258c11d921b098b214127c352a85
nervo <nervo@nervo.net>
2015-03-01 19:34:10 +0100

    Read bowercomponents dir in .bowerrc if exists

--- layouts/bower.js
+++ layouts/bower.js
@@ -1,12 +1,31 @@
 'use strict';
 
 
+var
+    fs = require('fs');
+
+
 module.exports = function(assets)
 {
-    // Library Patterns
-    assets
-        .addLibraryPattern({
-            path:        'bower_components',
-            description: 'Bower components'
-        });
+    var
+        path = 'bower_components';
+
+    // Try to find bower components dir in .bowerrc
+    fs.readFile(assets.fileSystem.getPath('.bowerrc'), 'utf8', function(error, data) {
+        if (!error) {
+            try {
+                data = JSON.parse(data);
+                if (data.directory) {
+                    path = data.directory;
+                }
+            } catch(exception) {}
+        }
+
+        // Library Patterns
+        assets
+            .addLibraryPattern({
+                path:        path,
+                description: 'Bower components'
+            });
+    });
 };
