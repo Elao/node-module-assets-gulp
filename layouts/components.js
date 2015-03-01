09a95d50a6cc5e18e69d287fd889ef69aed08cd5
nervo <nervo@nervo.net>
2015-03-01 19:33:41 +0100

    Update var name

--- layouts/components.js
+++ layouts/components.js
@@ -11,6 +11,6 @@ module.exports = function(assets, options)
     // Library Patterns
     assets
         .addLibraryPattern({
-            dir: options.dir
+            bundleDir: options.dir
         });
 };
